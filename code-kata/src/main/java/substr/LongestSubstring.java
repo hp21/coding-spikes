ad89eaf05baf3df8b6994642bd5db96773afc004
Peter Horvath <peter.horvath@lhsystems.com>
2014-11-04 22:46:11 +0100

    Longest common substring calculator added

---
+++ code-kata/src/main/java/substr/LongestSubstring.java
@@ -0,0 +1,34 @@
+package substr;
+
+/**
+ * Created by U292148 on 2014.11.04..
+ */
+public class LongestSubstring {
+
+    public String longestSubstring(String text, String pattern) {
+
+        final int textLength = text.length();
+        final int patternLength = pattern.length();
+
+        int subStringLength = -1;
+        int index = -1;
+
+        final int[][] array = new int[patternLength][textLength];
+
+        for (int row = 0; row < patternLength; row++) {
+            for (int col = 0; col < textLength; col++) {
+                if (pattern.charAt(row) == text.charAt(col)) {
+                    final int newLen = row > 0 && col > 0 ? array[row - 1][col - 1] + 1 : 1;
+                    array[row][col] = newLen;
+                    if (newLen > subStringLength) {
+                        subStringLength = newLen;
+                        index = col;
+                    }
+                }
+            }
+        }
+
+        System.out.printf("len: %d, index=%d\n", subStringLength, index);
+        return subStringLength > -1 ? text.substring(index - subStringLength + 1, index + 1) : "";
+    }
+}
---
+++ code-kata/src/test/java/substr/LongestSubstringTest.java
@@ -0,0 +1,47 @@
+package substr;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LongestSubstringTest {
+
+    private LongestSubstring finder;
+
+    @Before
+    public void setUp() {
+        finder = new LongestSubstring();
+    }
+
+    @Test
+    public void testLongestSubstring() throws Exception {
+        String actual = finder.longestSubstring("asd", "q");
+        assertThat(actual, equalTo(""));
+        //
+        actual = finder.longestSubstring("asd", "a");
+        assertThat(actual, equalTo("a"));
+                                         //
+        actual = finder.longestSubstring("asd", "as");
+        assertThat(actual, equalTo("as"));
+
+        actual = finder.longestSubstring("asd", "asd");
+        assertThat(actual, equalTo("asd"));
+
+        actual = finder.longestSubstring("asd", "asda");
+        assertThat(actual, equalTo("asd"));
+
+        actual = finder.longestSubstring("asd", "cer");
+        assertThat(actual, equalTo(""));
+
+        actual = finder.longestSubstring("asdaqweasdqweraa", "qwe");
+        assertThat(actual, equalTo("qwe"));
+
+        actual = finder.longestSubstring("asdaqweasdqweraa", "qwezz");
+        assertThat(actual, equalTo("qwe"));
+
+        actual = finder.longestSubstring("asdaqweasdqweraa", "bbqwezz");
+        assertThat(actual, equalTo("qwe"));
+    }
+}
