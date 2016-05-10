dc5853e62856425de7be87b8e0f22068d9017dc1
Peter Horvath <peter.horvath@lhsystems.com>
2016-05-10 20:52:50 +0200

    simon (1-1/2)(1-1/4)(1-1/8)

--- math/src/main/java/sp/Infimum.java
+++ math/src/main/java/sp/Infimum.java
@@ -4,7 +4,7 @@ package sp;
  * Created by U292148 on 2016.05.10..
  */
 public class Infimum {
-    private static final int RRR = 14;
+    private static final int RRR = 24;
     private static int num = RRR;
 
     public static void main(String[] args) {
@@ -15,10 +15,10 @@ public class Infimum {
 
         double sum = 1.0;
 
-        for (double n = 2.0; n < num; n++) {
-            sum = sum * (1 - (1 / n));
+        for (int n = 2; n < num; n++) {
+            sum = sum * (1 - (1 / (Math.pow(2, n))));
         }
 
-        System.out.printf("Sum: %1.12f\n", sum);
+        System.out.printf("Sum: %1.22f\n", sum);
     }
 }
