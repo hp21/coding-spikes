1f4c8c629150b60033222853e8a4a3030351c268
Peter Horvath <peter.horvath@lhsystems.com>
2016-05-11 08:21:08 +0200

    changed to bigdecimal

--- math/src/main/java/sp/Infimum.java
+++ math/src/main/java/sp/Infimum.java
@@ -1,10 +1,12 @@
 package sp;
 
+import java.math.BigDecimal;
+
 /**
  * Created by U292148 on 2016.05.10..
  */
 public class Infimum {
-    private static final int RRR = 24;
+    private static final int RRR = 18;
     private static int num = RRR;
 
     public static void main(String[] args) {
@@ -13,12 +15,13 @@ public class Infimum {
 
     public void run(final int num) {
 
-        double sum = 1.0;
+        BigDecimal sum = BigDecimal.valueOf(1.0);
 
         for (int n = 2; n < num; n++) {
-            sum = sum * (1 - (1 / (Math.pow(2, n))));
+            BigDecimal multiplier = BigDecimal.valueOf(1.0 - 1.0 / Math.pow(2, n));
+            sum = sum.multiply(multiplier);
         }
 
-        System.out.printf("Sum: %1.22f\n", sum);
+        System.out.printf("Sum: %1.42f\n", sum);
     }
 }
